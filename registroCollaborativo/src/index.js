85ab8fdf8230bb4c7d1455248eaea1788e686fbe
SalmurDev <filippo.bortolo@outlook.com>
2023-06-06 11:25:14 +0200

    group server finished

--- registroCollaborativo/src/index.js
+++ registroCollaborativo/src/index.js
@@ -5,10 +5,12 @@ const app = express()
 app.use(bodyParser.json())
 const port = 3000
 
-
+app.get('/', students.welcome)
 app.get('/digitazon/2023/02/group/2/students', students.get)
 app.get('/digitazon/2023/02/students', students.getAll)
 app.put('/digitazon/2023/02/group/:id/students', students.addGroup)
+app.delete('/digitazon/2023/02/students/last', students.deleteLast)
+app.delete('/digitazon/2023/02/students', students.deleteAll)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- registroCollaborativo/src/studentRoutes.mjs
+++ registroCollaborativo/src/studentRoutes.mjs
@@ -3,28 +3,54 @@ import studentsGroup2 from '../db/studentsGroup2.json' assert { type: 'json' }
 import students from '../db/students.json' assert { type: 'json' }
 import axios from 'axios'
 
-const DB_PATH = './db/studentsGroup2.json'
 const DB_PATH_ALL = './db/students.json'
+
+const GROUPS = {
+    1: 'https://5e81-2001-b07-a9a-89a8-fc69-90ae-c7c4-8dbc.ngrok-free.app/digitazon/2023/02/group/1/students',
+    2: 'http://localhost:3000/digitazon/2023/02/group/2/students',
+    3: 'https://4aac-151-33-19-106.ngrok-free.app/digitazon/2023/02/group/3/students',
+    4: 'https://f151-93-41-116-144.ngrok-free.app/digitazon/2023/02/group/4/students'
+}
 
 let NEXT_ID = Object
     .keys(students)
     .reduce((biggest, id) => biggest > id ? biggest : id, 0)
 
+export const welcome = (req, res) => {
+    res.send('<h1>Welcome to group 2</h1>')
+}
+
 export const get = (req, res) => {
-    res.send(students)
+    res.send(studentsGroup2)
 }
 export const getAll = (req, res) => {
-    res.send(studentsGroup2)
+    res.send(students)
 }
 
 export const addGroup = async (req, res) => {
-    const group = await axios.get(`http://localhost:3000/digitazon/2023/02/group/${req.params.id}/students`)
-    console.log(Object.keys(group.data).length);
-    for (let i = 1; i <= Object.keys(group.data).length; i++) {
-        console.log(group.data[i]);
-        NEXT_ID++
-        students[NEXT_ID] = group.data[i]
+    const group = await axios.get(`${GROUPS[req.params.id]}`)
+    if (Array.isArray(group.data)) {
+        for (let i = 0; i < group.data.length; i++) {
+            students.push(group.data[i])
+        }
+        await fs.writeFile(DB_PATH_ALL, JSON.stringify(students, null, '  '))
+        res.status(200).send({ message: 'group added' })
+        return
+    }
+    res.status(400).send({ error: true })
+}
+
+export const deleteLast = async (req, res) => {
+    students.pop()
+    await fs.writeFile(DB_PATH_ALL, JSON.stringify(students, null, '  '))
+    res.status(200).send({ message: 'student deleted' })
+}
+
+export const deleteAll = async (req, res) => {
+    let currentLength = students.length
+    for (let i = 0; i < currentLength; i++) {
+        students.pop()
     }
     await fs.writeFile(DB_PATH_ALL, JSON.stringify(students, null, '  '))
-    res.status(200).send({ message: 'group added' })
+    res.status(200).send({ message: 'students deleted' })
 }
